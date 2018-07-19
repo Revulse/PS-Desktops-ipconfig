426cea6d4a6f94dd8cebf0afe6b83617723d5b70
Revulse <jdegolyer@appliedtechres.com>
2018-07-19 10:38:39 -0400

    Initial Commit
    Initial Commit

---
+++ Desktops-ipconfig.ps1
@@ -0,0 +1,14 @@
+﻿Function CreateArray {
+
+    $computer_names = Get-ADGroupmember -Identity "CR Desktops" | select -expandproperty name
+   
+        return $computer_names
+}
+
+ForEach ($Desktop in CreateArray) {
+
+    $Date = Get-Date -UFormat "%Y_%m_%d"
+    $Filename = $Desktop + '   ' + $Date
+
+            Invoke-Command -ComputerName $Desktop -ScriptBlock {ipconfig /all} >> "\\atrdc2\scripts\PowerShell\logs\ipconfig-Logs\$Filename.txt"
+            }
